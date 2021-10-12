6f443282522a68974b7ec5a2c5b12c60d6f70b40
AM <austincmckee@gmail.com>
2021-10-12 11:40:37 +0900

    production commit

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+//import { composeWithDevTools } from 'redux-devtools-extension'
 
 import chartReducer from './reducers/chartReducer'
 import newsReducer from './reducers/newsReducer'
@@ -11,11 +11,19 @@ const reducer = combineReducers({
   news: newsReducer,
 })
 
+
+/*  Devtools commented out for production commit
 const store = createStore(
   reducer, 
   composeWithDevTools(
     applyMiddleware(thunk)
   )
+)
+*/
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk)
 )
 
 
